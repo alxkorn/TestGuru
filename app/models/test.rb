4cd162d71e9ffa663ac41cd3b1481e5e3e36642b
alxkorn <akorneyev10@yandex.ru>
2019-10-10 18:32:39 +0300

    Added sort_titles_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.sort_titles_by_category(category_title)
+    category_id = Category.find_by(title: category_title)
+    where(category_id: category_id).order(title: :desc).pluck(:title)
+  end
 end
