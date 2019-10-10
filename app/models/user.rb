d814505a38b8e4351625dffa50a2a480a72a3aa7
alxkorn <akorneyev10@yandex.ru>
2019-10-10 18:26:05 +0300

    Adeed passed_tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  def passed_tests_by_level(level)
+    test_ids = PassedTest.where(user_id: id).pluck(:test_id)
+    Test.where(id: test_ids, level: level)
+  end
 end
