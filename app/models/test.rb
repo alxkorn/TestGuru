38e96f4d4f44255e4fed3ff14b37ab84e8b09a6f
alxkorn <akorneyev10@yandex.ru>
2019-10-12 17:10:32 +0300

    Added associations to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,12 @@
 
 class Test < ApplicationRecord
   def self.sort_titles_by_category(category_title)
-    # joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category_title).order(title: :desc).pluck(:title)
     joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category_title }).order(title: :desc).pluck(:title)
   end
+
+  belongs_to :category
+  has_many :questions
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id' #author вместо user. чтобы было явное логическое отличие между users и user
 end
