b9858f353282f4ba093c11d49666eceae5480cd4
alxkorn <akorneyev10@yandex.ru>
2019-10-13 13:35:32 +0300

    Reworked methods using associations, added dpendent behaviour

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  def self.sort_titles_by_category(category_title)
-    joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category_title }).order(title: :desc).pluck(:title)
-  end
-
   belongs_to :category
-  has_many :questions
-  has_many :passed_tests
-  has_many :users, through: :passed_tests
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id' #author вместо user. чтобы было явное логическое отличие между users и user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :questions, dependent: :destroy
+  has_many :passed_tests, dependent: :destroy
+  has_many :users, through: :passed_tests, dependent: :destroy
+
+  def self.sort_titles_by_category(category_title)
+    joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
+  end
 end
