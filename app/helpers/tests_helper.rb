aad8be65585933c0d8b8dbeef3a289fa15da5f23
alxkorn <akorneyev10@yandex.ru>
2019-10-23 21:04:21 +0300

    Added test_level and test_header helpers

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 module TestsHelper
+  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+
   def questions_amount(test)
     test.questions.count
+  end
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
+  end
+
+  def test_header(test)
+    if test.new_record?
+      'Create new Test'
+    else
+      "Edit #{test.title} Test"
+    end
   end
 end
