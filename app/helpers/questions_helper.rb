5d495d3a115e223edda6f1dcb181bc72a90b6a3d
alxkorn <akorneyev10@yandex.ru>
2019-10-23 15:18:22 +0300

    Added helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_header
+    case params[:action]
+    when 'new'
+      "Create new #{@test.title} Question"
+    when 'edit'
+      "Edit #{@question.test.title} Question"
+    end
+  end
 end
