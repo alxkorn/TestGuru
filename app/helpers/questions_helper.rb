d5d644b39b60b0dfba9a249e018aefaf444acf4d
alxkorn <akorneyev10@yandex.ru>
2019-10-23 21:01:41 +0300

    question_header now accepts question parameter

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,9 @@
 module QuestionsHelper
-  def question_header
-    case params[:action]
-    when 'new'
-      "Create new #{@test.title} Question"
-    when 'edit'
-      "Edit #{@question.test.title} Question"
+  def question_header(question)
+    if question.new_record?
+      "Create new #{question.test.title} Question"
+    else
+      "Edit #{question.test.title} Question"
     end
   end
 end
