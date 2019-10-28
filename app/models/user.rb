90fbc713d7113f2426cb6aa91ae2111fa446d3aa
alxkorn <akorneyev10@yandex.ru>
2019-10-28 19:01:23 +0300

    Added auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include Auth
+
   enum role: { user: 0, creator: 1 }
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :created_tests, class_name: 'Test', dependent: :destroy
 
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
   scope :authors, -> { where(role: :creator) }
-  # validates :role, inclusion: { in: roles.keys }
 
   def passed_tests_with_level(level)
     tests.with_level(level)
