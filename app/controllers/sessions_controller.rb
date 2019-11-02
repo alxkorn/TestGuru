ed06e3c7f936b1ec5db02a054b62a71237668c28
alxkorn <akorneyev10@yandex.ru>
2019-11-02 15:32:47 +0300

    Fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      if session[:wanted_path]
-        redirect_to session[:wanted_path]
-        session.delete(:wanted_path)
-      else
-        redirect_to root_path
-      end
+      redirect_to session.delete(:wanted_path) || root_path
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
       render :new
