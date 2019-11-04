cfaa667d98bdaab616cde50ae06c7742701c2df1
alxkorn <akorneyev10@yandex.ru>
2019-11-04 14:49:55 +0300

    Fixed validations for Admin, added User#admin?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', dependent: :destroy
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -11,21 +13,15 @@ class User < ApplicationRecord
          :trackable,
          :confirmable
 
-  # enum role: { user: 0, creator: 1 }
-  has_many :test_passages, dependent: :destroy
-  has_many :tests, through: :test_passages, dependent: :destroy
-  has_many :created_tests, class_name: 'Test', dependent: :destroy
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  # has_secure_password
-
-  # scope :authors, -> { where(role: :creator) }
-
   def passed_tests_with_level(level)
     tests.with_level(level)
   end
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
+  end
+
+  def admin?
+    is_a?(Admin)
   end
 end
