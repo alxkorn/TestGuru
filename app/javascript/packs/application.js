5314e9791f68bc61eb8736de40ce9143199815c4
alxkorn <akorneyev10@yandex.ru>
2019-11-09 19:37:03 +0300

    Added progressbar to test passage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("jquery")
 require("utilities/sorting")
 require("utilities/check_password_confirmation")
 require("utilities/form_inline")
+require("utilities/progress_bar")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
