9b7498cd8d7eba93d5f4625cab4d8dc27ff7430d
alxkorn <akorneyev10@yandex.ru>
2019-11-14 21:48:39 +0300

    Created all methods in Admin::BadgesController

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,8 +1,57 @@
+# frozen_string_literal: true
+
 class Admin::BadgesController < Admin::BaseController
+  before_action :set_badge, only: %i[edit update show destroy]
+  before_action :set_badge_type, only: %i[new]
 
   def index
     @badges = Badge.all
   end
 
-  
+  def choose_type; end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+
+    if @badge.save
+      redirect_to admin_badges_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badge_path(@badge)
+    else
+      redirect_to :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
+  private
+
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:title, :type, :icon_path, :rule_value)
+  end
+
+  def set_badge_type
+    @badge_type = params[:type]
+  end
 end
