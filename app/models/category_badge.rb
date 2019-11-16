fdf46191db7465aa3d4ab197c4caab9e5add45f0
alxkorn <akorneyev10@yandex.ru>
2019-11-16 13:46:29 +0300

    Fixed queries for badges conditions

--- app/models/category_badge.rb
+++ app/models/category_badge.rb
@@ -12,8 +12,14 @@ class CategoryBadge < Badge
   def check_condition(test_passage)
     return false unless test_passage.test.category_id == category_id
 
-    passed_category_tests_count = test_passage.user.test_passages.where(passed: true).joins(:test).where('tests.category_id=?', category_id).count
-    Test.joins(:category).where(category_id: category_id).count <= passed_category_tests_count
+    # passed_category_tests_count = test_passage.user.test_passages.where(passed: true).joins(:test).where('tests.category_id=?', category_id).count
+    # Test.joins(:category).where(category_id: category_id).count <= passed_category_tests_count
+    num_tests_of_category = Test.joins(:category).where(category_id: category_id).count
+    passage_chart = test_passage.user.test_passages.where(passed: true).joins(:test).where('tests.category_id=?', category_id).group(:test_id).count
+    amount_condition = num_tests_of_category == passage_chart.keys.size
+    all_condition = passage_chart.values.uniq.count == 1
+
+    amount_condition && all_condition
   end
 
   def description
--- app/models/level_badge.rb
+++ app/models/level_badge.rb
@@ -12,7 +12,13 @@ class LevelBadge < Badge
   def check_condition(test_passage)
     return false unless test_passage.test.level == level
 
-    Test.where(level: level).count <= test_passage.user.tests.where(level: level).count
+    # Test.where(level: level).count <= test_passage.user.tests.where(level: level).count
+    num_tests_of_level = Test.where(level: level).count
+    passage_chart = test_passage.user.test_passages.where(passed: true).joins(:test).where('tests.level=?', level).group(:test_id).count
+    amount_condition = num_tests_of_level == passage_chart.keys.size
+    all_condition = passage_chart.values.uniq.count == 1
+
+    amount_condition && all_condition
   end
 
   def description
